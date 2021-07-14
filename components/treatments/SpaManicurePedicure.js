ae41bb79b4ad5c4145c07e8d3dd212035ba3e29f
Martins Siksnis <marty@Martinss-MacBook-Pro.local>
2021-07-15 00:59:16 +0200

    Spelling...

--- components/treatments/SpaManicurePedicure.js
+++ components/treatments/SpaManicurePedicure.js
@@ -8,19 +8,18 @@ export default function SpaManicurePedicure({ blackBtnTxt }) {
         SPA Manicure and Pedicure
       </h1>
       <h2 className="text-lg lg:text-xl opacity-70 poppins font-light mx-6 lg:mx-40">
-        Rene og godt vedlikeholdte hender og ben gir et godt inntrykk.
-        Spa-manikyrene og pedikyrene våre er unikt designet for å fjerne død
-        huden gjennom massering med peeling og fuktighetsgivende, og
-        neglebåndsbehandlinger som: aromatisk saltskrubb, fuktighetsgivende
-        maske, dampet varmt håndklepakning, massasje med varm stein med
-        luksuriøse kremer og oljer eller parafin. Massasje fra en profesjonell
-        manikyr eller pedikyr hjelper til med å gi utrolig avslapning og hjelper
-        også til å øke blodstrømmen. Våre spa mani-pedi-behandlinger vil gi deg
-        et avslappende sinn og avlaste deg fra stress og spenninger samtidig som
-        du får nydelige negler og vakkert preparerte hender og føtter! Vi
-        steriliserer utstyret vårt i varmluftsterilisator ved 250 grader for å
-        beskytte våre kunder mot potensielle risikoer som kan føre til
-        infeksjon.
+        Rene og godt vedlikeholdte hender og ben gir et godt inntrykk. Spa
+        manikyr og pedikyr er unikt designet for å fjerne død hud ved hjelp av
+        peeling, fuktighetsgivende massasje og neglebåndsbehandlinger som:
+        aromatisk saltskrubb, fuktighetsgivende maske, dampet varmt
+        håndklepakning, massasje med varm stein med luksuriøse kremer og oljer
+        eller parafin. Massasje fra en profesjonell manikyr eller pedikyr
+        hjelper til med å gi utrolig avslapning og hjelper også til å øke
+        blodstrømmen. Våre spa mani-pedi-behandlinger vil gi deg et avslappende
+        sinn og avlaste deg fra stress og spenninger samtidig som du får
+        nydelige negler og vakkert preparerte hender og føtter! Vi steriliserer
+        utstyret vårt i varmluftsterilisator ved 250 grader for å beskytte våre
+        kunder mot potensielle risikoer som kan føre til infeksjon.
       </h2>
       <div className="flex justify-center mb-10 lg:mb-14">
         <button className="mt-6 lg:mt-10 border border-black w-36 lg:w-72 mr-6 lg:mr-10 py-1 lg:py-2 bg-black/90 text-white text-sm lg:text-lg hover:bg-white hover:text-black/90 rounded shadow-lg">
