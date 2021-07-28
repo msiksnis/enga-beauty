1a35d5a833b5c8daba50566e98aaff77f16d3a3f
Martins Siksnis <marty@Martinss-MacBook-Pro.local>
2021-07-28 22:29:35 +0200

    Insta Gallery

--- components/InstaFeed.js
+++ components/InstaFeed.js
@@ -1,13 +1,34 @@
-import InstagramFeed from "react-ig-feed";
-import "react-ig-feed/dist/index.css";
+import { InstagramGallery } from "instagram-gallery";
 
 export default function InstaFeed() {
   return (
-    <div className="flex mx-0 lg:mx-20 mt-20 lg:mt-40">
-      <InstagramFeed
-        token="IGQVJYakFEZA29NQkJvazc5NVc1UmQyVWU1bkZA3MmE5MXdOQW83cVRGUzRZAVUx0RHVkU0tjdXZAhdjRDVzlhVXRQVEIweHN0UDZAHOW05VnpmbEllM05BU0VaeHZAkNkZArV0ZAWSkxPek9HVUtpc1pDMUNJawZDZD"
-        counter="6"
-      />
+    <div className="flex justify-center mx-0 lg:mx-20 mt-20 lg:mt-40">
+      <div className="flex flex-col">
+        <h1 className="flex justify-center text-xl md:text-2xl poppins mb-10 lg:mb-14">
+          Follow us on&nbsp;
+          <a
+            href="https://www.instagram.com/enga_beauty_ba10/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
+          </a>
+        </h1>
+        <div className="hidden md:flex">
+          <InstagramGallery
+            className="instagram-gallery instagram-item instagram-image"
+            accessToken="IGQVJYakFEZA29NQkJvazc5NVc1UmQyVWU1bkZA3MmE5MXdOQW83cVRGUzRZAVUx0RHVkU0tjdXZAhdjRDVzlhVXRQVEIweHN0UDZAHOW05VnpmbEllM05BU0VaeHZAkNkZArV0ZAWSkxPek9HVUtpc1pDMUNJawZDZD"
+            count={9}
+          />
+        </div>
+        <div className="md:hidden">
+          <InstagramGallery
+            className="instagram-gallery instagram-item instagram-image"
+            accessToken="IGQVJYakFEZA29NQkJvazc5NVc1UmQyVWU1bkZA3MmE5MXdOQW83cVRGUzRZAVUx0RHVkU0tjdXZAhdjRDVzlhVXRQVEIweHN0UDZAHOW05VnpmbEllM05BU0VaeHZAkNkZArV0ZAWSkxPek9HVUtpc1pDMUNJawZDZD"
+            count={8}
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- pages/testSlider.js
+++ pages/testSlider.js
@@ -1,9 +1,15 @@
 import TestHeader from "../components/test/TestHeader";
+import { InstagramGallery } from "instagram-gallery";
 
 export default function testSlider() {
   return (
     <div>
       <TestHeader />
+      <InstagramGallery
+        className="instagram-gallery instagram-item instagram-image"
+        accessToken="IGQVJYakFEZA29NQkJvazc5NVc1UmQyVWU1bkZA3MmE5MXdOQW83cVRGUzRZAVUx0RHVkU0tjdXZAhdjRDVzlhVXRQVEIweHN0UDZAHOW05VnpmbEllM05BU0VaeHZAkNkZArV0ZAWSkxPek9HVUtpc1pDMUNJawZDZD"
+        count={12}
+      />
     </div>
   );
 }
