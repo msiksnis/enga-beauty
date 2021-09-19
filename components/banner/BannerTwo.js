d980097329589b0a1d58f7b6dd836f62022283dd
Martins Siksnis <marty@Martinss-MacBook-Pro.local>
2021-09-19 22:04:47 +0200

    Modified banner

--- components/banner/BannerTwo.js
+++ components/banner/BannerTwo.js
@@ -4,17 +4,23 @@ import Link from "next/link";
 
 export default function BannerTwo() {
   return (
-    <div className="mt-14 lg:mt-36 lg:mx-20">
+    <div className="lg:mt-36 lg:mx-20">
       <ToTop />
-      <div className="flex justify-center">
-        <Link href="/">
-          <Image
-            className="object-cover"
-            src="/images/Carousel1.jpg"
-            width={1920}
-            height={950}
-          />
-        </Link>
+      <div className="hidden lg:flex justify-center">
+        <Image
+          className="object-cover"
+          src="/images/Carousel1.jpg"
+          width={1920}
+          height={950}
+        />
+      </div>
+      <div className="lg:hidden flex justify-center mt-20">
+        <Image
+          className="object-cover"
+          src="/images/Carousel1.jpg"
+          width={450}
+          height={300}
+        />
       </div>
     </div>
   );
