c1e418c2db1298a4f439a6798b6aba5b104c9194
Martins Siksnis <marty@Martinss-MacBook-Pro.local>
2021-10-24 21:30:41 +0200

    Remade scroll to top component

--- components/ToTop.js
+++ components/ToTop.js
@@ -1,36 +1,33 @@
 import { useEffect, useState } from "react";
 import { VscChevronUp } from "react-icons/vsc";
-import { Link } from "react-scroll";
 
 export default function ToTop() {
   const [toTop, setToTop] = useState(false);
 
-  const backToTop = () => {
-    if (window.scrollY >= 500) {
-      setToTop(true);
-    } else {
-      setToTop(false);
-    }
-  };
-
   useEffect(() => {
-    window.addEventListener("scroll", backToTop);
-    return () => window.removeEventListener("scroll", backToTop);
-  });
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 500) {
+        setToTop(true);
+      } else {
+        setToTop(false);
+      }
+    });
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
   return (
     <div className="hidden lg:inline">
-      <Link to="top" spy={true} smooth={true} offset={0} duration={500}>
-        <div
-          className={
-            toTop
-              ? "border border-gray-800 opacity-70 rounded-full p-1 cursor-pointer right-7 bottom-24 fixed"
-              : "hidden"
-          }
+      {toTop && (
+        <button
+          onClick={scrollToTop}
+          className="border border-gray-800 opacity-70 rounded-full p-1 cursor-pointer right-7 bottom-24 fixed"
         >
           <VscChevronUp />
-        </div>
-      </Link>
+        </button>
+      )}
     </div>
   );
 }
