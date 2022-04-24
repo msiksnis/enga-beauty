71b36e5f945335ad250d8bd762f1606f153dab32
Martins Siksnis <marty@Martinss-MacBook-Pro.local>
2022-04-24 21:59:19 +0200

    adding poster

--- components/Banner/BannerTwo.js
+++ components/Banner/BannerTwo.js
@@ -7,7 +7,13 @@ export default function BannerTwo() {
     <div className="mt-24 overflow-scroll md:mt-44 xl:px-40">
       <div className="relative">
         <div className="flex justify-center bg-cover">
-          <video ref={videoRef} autoPlay loop muted>
+          <video
+            ref={videoRef}
+            poster="/images/video/wavesImage.png"
+            autoPlay
+            loop
+            muted
+          >
             <source src="/images/video/waves-hd.mp4" />
           </video>
           <div className="font-poiret absolute top-[2vh] md:top-[6vh] xl:top-[8vh]">
