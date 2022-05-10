3b13fef808118a991c1296cb12860bfc26a55ca4
Martins Siksnis <marty@Martinss-MacBook-Pro.local>
2022-05-10 10:16:54 +0200

    testing video

--- components/Banner/BannerTwo.js
+++ components/Banner/BannerTwo.js
@@ -8,18 +8,22 @@ export default function BannerTwo() {
     <div className="mt-24 overflow-scroll md:mt-44 xl:px-40">
       <div className="relative">
         <div className="flex justify-center bg-cover">
+          <div
+            dangerouslySetInnerHTML={{
+              __html: `
           <video
             ref={videoRef}
             poster="/images/video/bgWaves.jpg"
-            src="/images/video/waves-hd.mp4"
             autoPlay
             playsinline
-            data-keepplaying
             loop
             muted
           >
             <source type="video/mp4" src="/images/video/waves-hd.mp4" />
           </video>
+          `,
+            }}
+          ></div>
           <div className="font-poiret absolute top-[2vh] md:top-[6vh] xl:top-[8vh]">
             <div className="flex flex-col items-center justify-center text-[13vw] font-bold leading-tight tracking-[0.25em] text-white md:text-[12vw] xl:text-[9vw]">
               <h1 className="uppercase">Love</h1>
