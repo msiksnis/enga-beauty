c91cbde7ea9d25782539535c031b6f5603348a65
Martins Siksnis <msiksnis@gmail.com>
2022-07-02 23:11:43 +0200

    fixingg

--- pages/test.js
+++ pages/test.js
@@ -1,7 +1,7 @@
 import AllTreatments from "../components/AllTreatments/AllTreatments";
 import Footer from "../components/Footer/Footer";
 import InstaFeed from "../components/InstaFeed/InstaFeed";
-import Testimonials from "../components/Testimonials/Testimonials";
+// import Testimonials from "../components/Testimonials/Testimonials";
 import BannerTwo from "../components/Banner/BannerTwo";
 import WordSwapper from "../components/WordSwapper/WordSwapper";
 import TestimonialSwiper from "../components/Testimonials/TestimonialSwiper";
