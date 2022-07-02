cc8c6f3e24db270fcb30d0ae6d8712ce5ea8543c
Martins Siksnis <msiksnis@gmail.com>
2022-07-02 23:08:32 +0200

    testing

--- components/Testimonials/Testimonials.js
+++ components/Testimonials/Testimonials.js
@@ -1,21 +1,21 @@
-import TestimonialCard from "./TestimonialCard";
-import { TestimonialData } from "./TestimonialData";
+// import TestimonialCard from "./TestimonialCard";
+// import { TestimonialData } from "./TestimonialData";
 
-export default function Testimonials() {
-  const data = TestimonialData;
+// export default function Testimonials() {
+//   const data = TestimonialData;
 
-  return (
-    <div className="">
-      <div className="mb-20 flex flex-col items-center justify-center text-gray-700">
-        <h1 className="font-poppins relative z-10 text-2xl capitalize">
-          What our clients say
-        </h1>
-      </div>
-      <div className="grid grid-flow-col md:gap-10 lg:gap-14">
-        {data?.map((data) => (
-          <TestimonialCard key={data.id} data={data} />
-        ))}
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="">
+//       <div className="mb-20 flex flex-col items-center justify-center text-gray-700">
+//         <h1 className="font-poppins relative z-10 text-2xl capitalize">
+//           What our clients say
+//         </h1>
+//       </div>
+//       <div className="grid grid-flow-col md:gap-10 lg:gap-14">
+//         {data?.map((data) => (
+//           <TestimonialCard key={data.id} data={data} />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
