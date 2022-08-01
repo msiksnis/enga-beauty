1a0f50d4e1b48294dca325303e7699a6adb4c399
Martins Siksnis <msiksnis@gmail.com>
2022-08-01 23:02:57 +0200

    small fix

--- pages/test.js
+++ pages/test.js
@@ -1,20 +1,3 @@
-import AllTreatments from "../components/AllTreatments/AllTreatments";
-import Footer from "../components/Footer/Footer";
-import InstaFeed from "../components/InstaFeed/InstaFeed";
-// import Testimonials from "../components/Testimonials/Testimonials";
-import BannerTwo from "../components/Banner/BannerTwo";
-import WordSwapper from "../components/WordSwapper/WordSwapper";
-import TestimonialSwiper from "../components/Testimonials/TestimonialSwiper";
-
 export default function TestPage() {
-  return (
-    <div className="mt-28 md:mt-44">
-      {/* <BannerTwo /> */}
-      {/* <AllTreatments /> */}
-      {/* <TestimonialSwiper /> */}
-      {/* <InstaFeed /> */}
-      {/* <WordSwapper /> */}
-      {/* <Footer /> */}
-    </div>
-  );
+  return <div className="mt-28 md:mt-44"></div>;
 }
