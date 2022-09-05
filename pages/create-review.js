a3db3380dcec3b7653e05d72e05aed0194c21e04
Martins Siksnis <msiksnis@gmail.com>
2022-09-05 21:02:41 +0200

    fixed

--- pages/create-review.js
+++
@@ -1,9 +0,0 @@
-import CreateTestimonial from "../components/Create/CreateTestimonial";
-
-export default function CreateReviewPage() {
-  return (
-    <div className="mt-32 md:mt-52">
-      <CreateTestimonial />
-    </div>
-  );
-}
