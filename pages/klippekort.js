d9b4344e0fe184f0984d9606e9d8f88beb846996
Martins Siksnis <msiksnis@gmail.com>
2022-11-22 22:22:51 +0100

    redeploy 4

--- pages/klippekort.js
+++ pages/klippekort.js
@@ -3,18 +3,15 @@ import Klippekort from "../components/Klippekort";
 import {
   fetchKlippekortPedicureFemale,
   fetchKlippekortPedicureMale,
-  fetchKlippekortManicureFemale,
-  fetchKlippekortManicureMale,
   fetchKlippekortVipperOgBryn,
 } from "../utils/fetchKlippekort";
 
 export default function KlippekortPage({
   klippekortPedicureFemale,
   klippekortPedicureMale,
-  klippekortManicureFemale,
-  klippekortManicureMale,
   klippekortVipperOgBryn,
 }) {
+  console.log(klippekortPedicureFemale);
   return (
     <>
       <Head>
@@ -23,8 +20,6 @@ export default function KlippekortPage({
       <Klippekort
         klippekortPedicureFemale={klippekortPedicureFemale}
         klippekortPedicureMale={klippekortPedicureMale}
-        klippekortManicureFemale={klippekortManicureFemale}
-        klippekortManicureMale={klippekortManicureMale}
         klippekortVipperOgBryn={klippekortVipperOgBryn}
       />
     </>
@@ -34,16 +29,12 @@ export default function KlippekortPage({
 export async function getStaticProps() {
   const klippekortPedicureFemale = await fetchKlippekortPedicureFemale();
   const klippekortPedicureMale = await fetchKlippekortPedicureMale();
-  const klippekortManicureFemale = await fetchKlippekortManicureFemale();
-  const klippekortManicureMale = await fetchKlippekortManicureMale();
   const klippekortVipperOgBryn = await fetchKlippekortVipperOgBryn();
 
   return {
     props: {
       klippekortPedicureFemale,
       klippekortPedicureMale,
-      klippekortManicureFemale,
-      klippekortManicureMale,
       klippekortVipperOgBryn,
     },
     revalidate: 10,
