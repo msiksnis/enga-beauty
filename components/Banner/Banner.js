49195ee1a0147cf4b9c935b674d0b4c8aa2b65eb
Martins Siksnis <msiksnis@gmail.com>
2023-01-31 22:13:35 +0100

    changed banner

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -1,11 +1,9 @@
 import BannerSix from "./BannerSix";
-import BannerTwo from "./BannerTwo";
 
 export default function Banner() {
   return (
     <>
-      {/* <BannerSix /> */}
-      <BannerTwo />
+      <BannerSix />
     </>
   );
 }
