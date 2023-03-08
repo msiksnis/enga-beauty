fe349c3ab9d75eca561688696873ce74c8a5c7a6
Martins Siksnis <msiksnis@gmail.com>
2023-03-08 22:29:07 +0100

    trying to deploy 11

---
+++ pages/behandlinger-priser/behandlinger.js
@@ -0,0 +1,68 @@
+import Head from "next/head";
+import VareBehandlinger from "../../components/Priser/VareBehandlinger";
+import {
+  fetchPedicureFemale,
+  fetchPedicureMale,
+} from "../../utils/fetchPedicureTreatments";
+import {
+  fetchManicureFemale,
+  fetchManicureMale,
+} from "../../utils/fetchManicureTreatments";
+import { fetchMicroblading } from "../../utils/fetchMicroblading";
+import { fetchHudpleie } from "../../utils/fetchHudpleie";
+import { fetchLashes } from "../../utils/fetchLashes";
+import { fetchBrows } from "../../utils/fetchBrows";
+
+export default function BehandlingerPage({
+  pedicureFemale,
+  pedicureMale,
+  manicureFemale,
+  manicureMale,
+  microblading,
+  hudpleie,
+  lashes,
+  brows,
+}) {
+  return (
+    <>
+      <Head>
+        <title>Atelier Beauté | Våre Behandlinger</title>
+      </Head>
+      <VareBehandlinger
+        pedicureFemale={pedicureFemale}
+        pedicureMale={pedicureMale}
+        manicureFemale={manicureFemale}
+        manicureMale={manicureMale}
+        microblading={microblading}
+        hudpleie={hudpleie}
+        lashes={lashes}
+        brows={brows}
+      />
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const pedicureFemale = await fetchPedicureFemale();
+  const pedicureMale = await fetchPedicureMale();
+  const manicureFemale = await fetchManicureFemale();
+  const manicureMale = await fetchManicureMale();
+  const microblading = await fetchMicroblading();
+  const hudpleie = await fetchHudpleie();
+  const lashes = await fetchLashes();
+  const brows = await fetchBrows();
+
+  return {
+    props: {
+      pedicureFemale,
+      pedicureMale,
+      manicureFemale,
+      manicureMale,
+      microblading,
+      hudpleie,
+      lashes,
+      brows,
+    },
+    revalidate: 10,
+  };
+}
