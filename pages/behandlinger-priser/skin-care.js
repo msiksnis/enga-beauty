3bad3620429e9ddc6d9ca83880f02a86bf6b9646
Martins Siksnis <msiksnis@gmail.com>
2023-03-08 22:15:57 +0100

    trying to deploy six

---
+++ pages/behandlinger-priser/skin-care.js
@@ -0,0 +1,25 @@
+// import Head from "next/head";
+// import Hudpleie from "../../components/Priser/Hudpleie";
+// import { fetchHudpleie } from "../../utils/fetchHudpleie";
+
+// export default function HudpleiePage({ hudpleie }) {
+//   return (
+//     <>
+//       <Head>
+//         <title>Atelier Beauté | Hudpleie</title>
+//       </Head>
+//       <Hudpleie hudpleie={hudpleie} />
+//     </>
+//   );
+// }
+
+// export async function getStaticProps() {
+//   const hudpleie = await fetchHudpleie();
+
+//   return {
+//     props: {
+//       hudpleie,
+//     },
+//     revalidate: 10,
+//   };
+// }
